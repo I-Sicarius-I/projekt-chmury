b90b08a7698308e42fbe123720a08aaaba4a5397
0gwiazda <wgwiazda@student.agh.edu.pl>
2023-12-11 16:24:58 +0100

    button style changes

--- frontend/simple-facebook/src/App.jsx
+++ frontend/simple-facebook/src/App.jsx
@@ -7,22 +7,44 @@ import { PostProvider } from './Context/PostsContext';
 import Navbar from './Components/Navbar';
 import { CommentProvider } from './Context/CommentContext';
 import { ReactionProvider } from './Context/ReactionContext';
+import { createTheme } from '@mui/system';
+import { ThemeProvider } from '@emotion/react';
 
 
 function App() 
 {
+  const theme = createTheme({
+    components:{
+      MuiButton: {
+        defaultProps: {
+          disableRipple: true,
+          variant: "contained"
+        },
+        styleOverrides: {
+          color: "white",
+          backgroundColor: "#13206e"
+        }
+      }
+    }
+  })
+
+
+
+  
   return (
     <UserProvider>
       <PostProvider>
         <CommentProvider>   
-          <ReactionProvider>       
-            <div className="App">
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<PostPage />}/>
-                <Route path="/users" element={<UserPage />}/>
-              </Routes>
-            </div>
+          <ReactionProvider>
+            <ThemeProvider theme={theme}>       
+              <div className="App">
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<PostPage />}/>
+                  <Route path="/users" element={<UserPage />}/>
+                </Routes>
+              </div>
+            </ThemeProvider>
           </ReactionProvider>
         </CommentProvider>
       </PostProvider>
